f9c5f3617509eaf2b309b07b98e5d1e059b4cf00
srh <sr.hannam@connect.qut.edu.au>
2019-12-04 07:43:05 +1000

    autopush:no commit message given

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CUDA_impl_2048pt_TukeyCooley_Iterative_FFT
+# CUDA implementation of 2048pt TukeyCooley Iterative FFT
+
+NB: This was an exercise in parallelizing an algorithm using CUDA. Making the Digital Music Analysis program run faster is the premise but it is an educational contrivance. In an industry setting, the optimal solution would be to use FFTW or cuFFT and focus on application and presentation layers at a higher level; this was done in a facile way with FFTW in order to compare a best-possible CPU only performance to the GPU performance.
+
+
---
+++ moded_DigitalMusicAnalysis_files/timefreq.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Numerics;
+using System.IO;
+using System.Runtime.InteropServices;
+using FFTWSharp;
+
+namespace DigitalMusicAnalysis
+{
+    public class timefreq
+    {
+        public float[][] timeFreqData;
+        public int wSamp;
+        public Complex[] twiddles;
+
+        public timefreq(float[] x, int windowSamp)
+        {
+            double pi = 3.14159265;
+            Complex i = Complex.ImaginaryOne;
+            this.wSamp = windowSamp;
+            twiddles = new Complex[wSamp];
+
+            //string freqfilename = @"C:\CAB401data\timefreq_out";
+            //FileStream freqoutFile = File.Create(freqfilename);
+            //BinaryWriter freqbinWrite = new BinaryWriter(freqoutFile);
+            
+            for (int ii = 0; ii < wSamp; ii++)
+            {
+                double a = 2 * pi * ii / (double)wSamp;
+                twiddles[ii] = Complex.Pow(Complex.Exp(-i), (float)a);
+            }
+
+            timeFreqData = new float[wSamp/2][];
+
+            int nearest = (int)Math.Ceiling((double)x.Length / (double)wSamp);
+            nearest = nearest * wSamp;
+
+            Complex[] compX = new Complex[nearest];
+            for (int kk = 0; kk < nearest; kk++)
+            {
+                if (kk < x.Length)
+                {
+                    compX[kk] = x[kk];
+                }
+                else
+                {
+                    compX[kk] = Complex.Zero;
+                }
+            }
+
+            int cols = 2 * nearest /wSamp;
+
+            for (int jj = 0; jj < wSamp / 2; jj++)
+            {
+                timeFreqData[jj] = new float[cols];
+            }
+
+            timeFreqData = stft(compX, wSamp);
+
+            //for (int ii = 0; ii < wSamp/2; ii++) {
+            //    for (int jj = 0; jj < cols; jj++) {
+            //        freqbinWrite.Write(timeFreqData[ii][jj]);
+            //    }
+            //}
+            //freqbinWrite.Flush();
+            //freqbinWrite.Close();
+            //long length = new System.IO.FileInfo(freqfilename).Length;
+            //Console.WriteLine("Write: length in elements {0}, length in bytes {1}",(cols*wSamp/2),length);
+            
+            //freqbinWrite.Dispose();
+        }
+
+        float[][] stft(Complex[] x, int wSamp)
+        {
+            int ii = 0;
+            int jj = 0;
+            int kk = 0;
+            int ll = 0;
+            int N = x.Length;
+            float fftMax = 0;
+            
+            float[][] Y = new float[wSamp / 2][];
+
+            // initialising the Y which will be returned
+            for (ll = 0; ll < wSamp / 2; ll++)
+            {
+                Y[ll] = new float[2 * (int)Math.Floor((double)N / (double)wSamp)];
+            }
+
+            Complex[] temp = new Complex[wSamp];
+            Complex[] tempFFT = new Complex[wSamp];
+
+            //fftw_complexarray temp, tempFFT;
+            //temp = new fftw_complexarray(wSamp);
+            //tempFFT = new fftw_complexarray(wSamp);
+
+            //IntPtr fplan;
+            //GCHandle hdin, hdout;
+            //hdin = GCHandle.Alloc(temp, GCHandleType.Pinned);
+            //hdout = GCHandle.Alloc(tempFFT, GCHandleType.Pinned);
+
+            //fplan = fftw.dft_1d(wSamp, hdin.AddrOfPinnedObject(), hdout.AddrOfPinnedObject(), fftw_direction.Forward, fftw_flags.Measure);
+            
+            // populating the Y[][] 
+            for (ii = 0; ii < 2 * Math.Floor((double)N / (double)wSamp) - 1; ii++)
+            {
+
+                // 1) grab 2048 chunks of time domain data
+                // 2) find fft of it
+                // 3) assign as a single column the result of fft to final Y[][]
+                for (jj = 0; jj < wSamp; jj++)
+                {
+                    temp[jj] = x[ii * (wSamp / 2) + jj];
+                }
+                tempFFT = fft(temp);
+                /* use basic single-threaded FFTW equivalent instead */
+               
+                
+
+
+                // create two unmanaged arrays, properly aligned
+                
+                //fftwf.execute(fplan);
+
+
+                for (kk = 0; kk < wSamp / 2; kk++)
+                {
+                    // we only need the magnitude data of the fft
+                    Y[kk][ii] = (float)Complex.Abs(tempFFT[kk]);
+
+                    // progressively find the fftMax used in normalisation
+                    if (Y[kk][ii] > fftMax)
+                    {
+                        fftMax = Y[kk][ii];
+                    }
+                }
+
+
+            }
+            //Console.WriteLine("fftMax: {0}", fftMax);
+            // normalise the resulting array Y[][]
+            for (ii = 0; ii < 2 * Math.Floor((double)N / (double)wSamp) - 1; ii++)
+            {
+                for (kk = 0; kk < wSamp / 2; kk++)
+                {
+                    Y[kk][ii] /= fftMax;
+                }
+            }
+            //fftwf.destroy_plan(fplan);
+            //hdin.Free();
+            //hdout.Free();
+            return Y;
+        }
+
+        Complex[] fft(Complex[] x)
+        {
+            int ii = 0;
+            int kk = 0;
+            int N = x.Length;
+
+            Complex[] Y = new Complex[N];
+
+            // NEED TO MEMSET TO ZERO?
+
+            if (N == 1)
+            {
+                Y[0] = x[0];
+            }
+            else{
+
+                Complex[] E = new Complex[N/2];
+                Complex[] O = new Complex[N/2];
+                /* early optimisation */
+                //Complex[] even = new Complex[N/2];
+                //Complex[] odd = new Complex[N/2];
+
+                for (ii = 0; ii < N; ii++)
+                {
+
+                    if (ii % 2 == 0)
+                    {
+                        E[ii / 2] = x[ii];
+                    }
+                    if (ii % 2 == 1)
+                    {
+                        O[(ii - 1) / 2] = x[ii];
+                    }
+                }
+
+                E = fft(E);
+                O = fft(O);
+
+                for (kk = 0; kk < N; kk++)
+                    Y[kk] = E[(kk % (N / 2))] + O[(kk % (N / 2))] * twiddles[kk * wSamp / N];
+            }
+
+           return Y;
+        }
+        
+    }
+}
